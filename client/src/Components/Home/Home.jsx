b557d60fb47f97ea3c23dcbfb80f26fd0fd4ade3
Rishab Goyal <rishabgoyal432@gmail.com>
2021-10-05 03:39:18 +0530

    vector

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './home.css';
-
+import landingVector from "../../assets/landing-vector.svg";
 import Header from '../partials/Header/Header';
 
 const Home = () => {
     return (
       <div className="">
         <Header/>
+        <div className="container mt-5 pt-5 pt-md-3">
+          <div className="row">
+            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center order-md-last">
+              <img src={landingVector} alt="" style={{width:"90%",height:"auto"}}/>
+            </div>
+            <div className="col-12 col-md-6">
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
 }
