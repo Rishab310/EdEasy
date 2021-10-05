0d0021b50ab258016b47580d8c4337136a89e7fb
Rishab Goyal <rishabgoyal432@gmail.com>
2021-10-05 20:41:42 +0530

    Footer fixed

--- client/src/Components/partials/Header/Header.js
+++ client/src/Components/partials/Header/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <>
-            <Link to="/" class="navbar-brand ms-5" href="#">EdEasy</Link>
+            <Link to="/" class="navbar-brand ms-5 fw-bold" href="#">EdEasy</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
             aria-expanded="false" aria-label="Toggle navigation">
