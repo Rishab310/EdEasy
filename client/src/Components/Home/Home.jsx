fe0dc1910d474e4d779e8ffb95b80862a7fa5d99
Rishab Goyal <rishabgoyal432@gmail.com>
2021-10-05 19:48:42 +0530

    Footer and alignment

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,7 +3,10 @@ import './Home.css';
 import landingVector from "../../assets/landing-vector.svg";
 import Header from '../partials/Header/Header';
 import FacebookIcon from '@material-ui/icons/Facebook';
-
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import CopyrightIcon from '@material-ui/icons/Copyright';
 const Home = () => {
     return (
       <div className="">
@@ -28,17 +31,27 @@ const Home = () => {
         </section>
         <div className="Footer mt-3">
             <div className="row p-0 m-0">
-                <div className="col-12 col-md-6 d-flex">
-                    <div className="Footer_Copyright pe-4 ms-5 py-2 fs-5">
-                      © EdEasy 2021
+                <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-start px-0 py-2">
+                    <div className="Footer_Copyright pe-2 pe-md-4 ms-0 ms-md-5 fw-bold">
+                      <CopyrightIcon style={{fontSize: "16px", marginTop: "-5px"}}/> EdEasy 2021
                     </div>
-                    <div className="ms-4 py-2 fs-5">
+                    <div className="ms-0 ms-md-4 ps-2 ps-md-0">
                       edeasy123@gmail.com
                     </div>
                 </div>
-                <div className="col-12 col-md-6 d-flex justify-content-end py-2">
+                <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end py-2">
+                  <span className="px-2">
                     <FacebookIcon />
-                    f t i e
+                  </span>
+                  <span className="px-2">
+                    <InstagramIcon/>
+                  </span>
+                  <span className="px-2">
+                    <TwitterIcon/>
+                  </span>
+                  <span className="px-2">
+                    <MailOutlineIcon/>
+                  </span>
                 </div>
             </div>
         </div>
