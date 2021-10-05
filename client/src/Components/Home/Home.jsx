4624189a091d71e1ec9d94317833fef895a9eb11
ManishDhameja <manishdhameja86@gmail.com>
2021-10-05 18:00:25 +0530

    landpage

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './home.css';
+import './Home.css';
 import landingVector from "../../assets/landing-vector.svg";
 import Header from '../partials/Header/Header';
 
@@ -7,18 +7,39 @@ const Home = () => {
     return (
       <div className="">
         <Header/>
-        <div className="container mt-5 pt-5 pt-md-3">
-          <div className="row">
-            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center order-md-last">
-              <img src={landingVector} alt="" style={{width:"90%",height:"auto"}}/>
-            </div>
-            <div className="col-12 col-md-6">
-              
-            </div>
+        <section id="hero">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <div>
+            <h2>Enhance your learning with</h2>
+            <h1><strong>EdEasy</strong></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.,<br/> 
+              Fuga provident eaque facere, sunt sint quis nostrum dolorum.</p>
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
           </div>
         </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img aos-init aos-ani0" data-aos="fade-left">
+          <img src={landingVector} class="img-fluid" alt="" />
+        </div>
+      </div>
+    </div>
+
+  </section>
       </div>
     );
-}
-
-export default Home
+  }
+  
+  export default Home
+  
+  {/* <div className="container mt-5 pt-5 pt-md-3">
+    <div className="row">
+      <div className="col-12 col-md-6 d-flex align-items-center justify-content-center order-md-last">
+        <img src={landingVector} alt="" style={{width:"90%",height:"auto"}}/>
+      </div>
+      <div className="col-12 col-md-6">
+        
+      </div>
+    </div>
+  </div> */}
