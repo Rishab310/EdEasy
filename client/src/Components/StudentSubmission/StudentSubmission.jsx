ab0a85d598983d904db731f2ed53de3b71f18cac
ManishDhameja <manishdhameja86@gmail.com>
2021-10-13 02:16:08 +0530

    assignmentstudent

---
+++ client/src/Components/StudentSubmission/StudentSubmission.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './StudentSubmission.css'
+
+const StudentSubmission = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex mt-5 fs-3 justify-content-left border-bot">
+                        Assignment - 01 DS-OS
+                    </div>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default StudentSubmission
