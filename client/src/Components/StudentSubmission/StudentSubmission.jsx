3848cef8ecb437181184c34ae9ba3dc769bf42d3
ManishDhameja <manishdhameja86@gmail.com>
2021-10-14 02:42:04 +0530

    assignmentstudent

--- client/src/Components/StudentSubmission/StudentSubmission.jsx
+++ client/src/Components/StudentSubmission/StudentSubmission.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './StudentSubmission.css'
 import Avatar from '@material-ui/core/Avatar';
 
 const StudentSubmission = () => {
+    let btn_class = '';
+    let text = '';
+    let btnClick = null;
+
+    const uploadFile = ()=>{
+        fileInput.current.click();
+        // setUploadState(1);
+    }
+    const submitFile = ()=>{
+        
+    }
+    const unSubmitFile = (e)=>{
+
+    }
+
+    const [uploadState, setUploadState] = useState(0);
+    switch (uploadState % 3) {
+        case 0: {
+            btnClick = uploadFile;
+            btn_class = 'btn btn-outline-primary fs-6';
+            text = 'Upload';
+        }
+            break;
+        case 1: {
+            btnClick = submitFile;
+            btn_class = 'btn btn-outline-success fs-6';
+            text = 'Submit';
+        }
+            break;
+        case 2: {
+            btnClick = unSubmitFile;
+            btn_class = 'btn btn-outline-danger fs-6';
+            text = 'Unsubmit';
+        }
+            break;
+    }
+
+    const [pdfFile, setPdfFile] = useState(null);
+    const [pdfFileError, setPdfFileError] = useState('');
+    const fileInput = useRef(null);
+    const [inputFile, setInputFile] = useState(null);
+
+    const onUploadClick = (e) =>{
+        setInputFile(e.target.files[0]);
+        setUploadState(1);
+    }
+    console.log(inputFile);
+
+    const fileType = ['application/pdf'];
+    const handlePdfFileChange = (e) => {
+        let selectedFile = e.target.files[0];
+        if (selectedFile) {
+            if (selectedFile && fileType.includes(selectedFile.type)) {
+                let reader = new FileReader();
+                reader.readAsDataURL(selectedFile);
+                reader.onloadend = (e) => {
+                    setPdfFile(e.target.result);
+                    setPdfFileError('');
+                }
+            }
+            else {
+                setPdfFile(null);
+                setPdfFileError('Please select valid pdf file');
+            }
+        }    
+            else{
+                console.log('select your file');
+            }
+        
+    }
+
     return (
         <div>
             <div className="container">
@@ -26,21 +97,21 @@ const StudentSubmission = () => {
                                         </div>
                                     </div>
                                     <div class="Assignment_Box_student d-flex flex-column justify-content-center px-3 py-2">
-                                    <div class="d-flex mb-2">
-                                        <div class="Avatar_Container">
-                                            <div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault">
-                                                M
-                                            </div>
-                                        </div>
-                                        <div class="Post_Author d-flex flex-column justify-content-center mx-3">
-                                            <div class="Post_AuthorName">
-                                                Manish Dhameja
+                                        <div class="d-flex mb-2">
+                                            <div class="Avatar_Container">
+                                                <div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault">
+                                                    M
                                                 </div>
-                                            <div class="Post_AdminName">
-                                                Admin
+                                            </div>
+                                            <div class="Post_Author d-flex flex-column justify-content-center mx-3">
+                                                <div class="Post_AuthorName">
+                                                    Manish Dhameja
+                                                </div>
+                                                <div class="Post_AdminName">
+                                                    Admin
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
                                         <div class="Assignment_Img">
                                             <img src="https://media.istockphoto.com/photos/health-care-billing-statement-with-stethoscope-picture-id1224851166?b=1&amp;k=20&amp;m=1224851166&amp;s=170667a&amp;w=0&amp;h=xBJfeOFCnBG5Z6zgI2OFicnvgMF-idwwu3TuRvtq1y8=" alt="" />
                                             <div class="Assignment_Name">
@@ -56,15 +127,21 @@ const StudentSubmission = () => {
                         <div className="col d-flex mt-4 pt-3 justify-content-left border-bot fs-5">
                             Your Submission
                         </div>
+
                         <div className="mt-3">
-                            <button type="button" class="btn btn-outline-primary fs-6"><strong>Upload Work</strong></button>
+                            <form className='form-group' >
+                                <input onChange={onUploadClick} className="display_pdf" ref={fileInput} type="file" name="upload" accept="application/pdf,application/vnd.ms-excel" />
+                                {pdfFileError && <div className='error-msg'>{pdfFileError}</div>}
+                                <br></br>
+                                <button onClick={btnClick} type="button" class={btn_class}><strong>{text}</strong></button>
+                            </form>
                         </div>
                     </div>
                 </div>
-            {/* </div> */}
+                {/* </div> */}
             </div>
-        </div>    
-    )        
+        </div>
+    )
 }
 
 export default StudentSubmission;
