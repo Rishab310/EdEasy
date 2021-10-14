5b3c32141216279cca42fadcfd074fd87e16edb4
ManishDhameja <manishdhameja86@gmail.com>
2021-10-14 21:15:04 +0530

    studentpage

--- client/src/Components/StudentSubmission/StudentSubmission.jsx
+++ client/src/Components/StudentSubmission/StudentSubmission.jsx
@@ -4,75 +4,103 @@ import Avatar from '@material-ui/core/Avatar';
 
 const StudentSubmission = () => {
     let btn_class = '';
+    let btn_class_1 = '';
     let text = '';
     let btnClick = null;
+    let btnClick_back = null;
 
-    const uploadFile = ()=>{
+    // const [pdfFile, setPdfFile] = useState([])
+
+    const uploadFile = () => {
         fileInput.current.click();
         // setUploadState(1);
     }
-    const submitFile = ()=>{
-        
+    const submitFile = () => {
+        setUploadState(2);
     }
-    const unSubmitFile = (e)=>{
-
+    const unSubmitFile = () => {
+        setUploadState(0);
     }
+    const [pdfFileError, setPdfFileError] = useState('');
 
     const [uploadState, setUploadState] = useState(0);
     switch (uploadState % 3) {
         case 0: {
             btnClick = uploadFile;
-            btn_class = 'btn btn-outline-primary fs-6';
+            btn_class = 'button_hover btn btn-outline-primary fs-6';
             text = 'Upload';
         }
             break;
         case 1: {
             btnClick = submitFile;
-            btn_class = 'btn btn-outline-success fs-6';
+            btnClick_back = uploadFile;
+            if(pdfFileError){
+                btn_class = 'btn btn-outline-success fs-6 d-flex mt-3 disabled';
+            }
+            else{
+                btn_class = 'btn btn-outline-success fs-6 d-flex mt-3';
+            }
+            btn_class_1 = 'button_hover btn btn-outline-primary fs-6 d-flex mt-3';
             text = 'Submit';
         }
             break;
         case 2: {
             btnClick = unSubmitFile;
-            btn_class = 'btn btn-outline-danger fs-6';
+            btn_class = 'btn btn-outline-danger fs-6 d-flex mx-auto mt-3';
             text = 'Unsubmit';
         }
             break;
     }
 
-    const [pdfFile, setPdfFile] = useState(null);
-    const [pdfFileError, setPdfFileError] = useState('');
+    // const [pdfFile, setPdfFile] = useState(null);
     const fileInput = useRef(null);
     const [inputFile, setInputFile] = useState(null);
 
-    const onUploadClick = (e) =>{
+    const onUploadClick = (e) => {
+        if(!e.target.files[0]){
+            return
+        }
         setInputFile(e.target.files[0]);
         setUploadState(1);
-    }
-    console.log(inputFile);
-
-    const fileType = ['application/pdf'];
-    const handlePdfFileChange = (e) => {
+        const fileType = ['application/pdf'];
         let selectedFile = e.target.files[0];
         if (selectedFile) {
             if (selectedFile && fileType.includes(selectedFile.type)) {
                 let reader = new FileReader();
                 reader.readAsDataURL(selectedFile);
                 reader.onloadend = (e) => {
-                    setPdfFile(e.target.result);
                     setPdfFileError('');
                 }
             }
             else {
-                setPdfFile(null);
                 setPdfFileError('Please select valid pdf file');
             }
-        }    
-            else{
-                console.log('select your file');
-            }
-        
+        }
     }
+    console.log(inputFile);
+
+    // const fileType = ['application/pdf'];
+    // const handlePdfFileChange = (e) => {
+    //     let selectedFile = e.target.files[0];
+    //     if (selectedFile) {
+    //         if (selectedFile && fileType.includes(selectedFile.type)) {
+    //             let reader = new FileReader();
+    //             reader.readAsDataURL(selectedFile);
+    //             reader.onloadend = (e) => {
+    //                 setPdfFile(e.target.result);
+    //                 setPdfFileError('');
+    //             }
+    //         }
+    //         else {
+    //             setPdfFile(null);
+    //             setPdfFileError('Please select valid pdf file');
+    //         }
+    //     }    
+    //         else{
+    //             console.log('select your file');
+    //         }
+
+    // }
 
     return (
         <div>
@@ -89,10 +117,10 @@ const StudentSubmission = () => {
                             <a href="https://media.istockphoto.com/photos/health-care-billing-statement-with-stethoscope-picture-id1224851166?b=1&amp;k=20&amp;m=1224851166&amp;s=170667a&amp;w=0&amp;h=xBJfeOFCnBG5Z6zgI2OFicnvgMF-idwwu3TuRvtq1y8=" target="_blank">
                                 <div class="d-flex flex-column">
                                     <div class="d-flex justify-content-between">
-                                        <div class="Assignment_Date">
+                                        <div class="Assignment_Date_student">
                                             11 October 2021
                                         </div>
-                                        <div class="Assignment_Time">
+                                        <div class="Assignment_Time_student">
                                             2:00 PM
                                         </div>
                                     </div>
@@ -115,7 +143,7 @@ const StudentSubmission = () => {
                                         <div class="Assignment_Img">
                                             <img src="https://media.istockphoto.com/photos/health-care-billing-statement-with-stethoscope-picture-id1224851166?b=1&amp;k=20&amp;m=1224851166&amp;s=170667a&amp;w=0&amp;h=xBJfeOFCnBG5Z6zgI2OFicnvgMF-idwwu3TuRvtq1y8=" alt="" />
                                             <div class="Assignment_Name">
-                                                Lab Assgn1.pdf
+                                                Lab_Assignment-1
                                             </div>
                                         </div>
                                     </div>
@@ -124,17 +152,54 @@ const StudentSubmission = () => {
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="col d-flex mt-4 pt-3 justify-content-left border-bot fs-5">
-                            Your Submission
-                        </div>
-
+                        {uploadState == 2 ?
+                          <div className="col d-flex mt-4 pt-3 justify-content-between border-bot fs-5">
+                              <div>
+                                Your Submission
+                            </div>
+                              <div className="mt-6 fs-6 fw-bold text-success">
+                                Submitted
+                            </div>
+                            </div>
+                        :
+                            <div className="col d-flex mt-4 pt-3 justify-content-left border-bot fs-5 col-fs-6">
+                                Your Submission
+                            </div>
+                        }
                         <div className="mt-3">
                             <form className='form-group' >
                                 <input onChange={onUploadClick} className="display_pdf" ref={fileInput} type="file" name="upload" accept="application/pdf,application/vnd.ms-excel" />
-                                {pdfFileError && <div className='error-msg'>{pdfFileError}</div>}
-                                <br></br>
-                                <button onClick={btnClick} type="button" class={btn_class}><strong>{text}</strong></button>
-                            </form> 
+                                {pdfFileError && <div className='error-msg text-danger'>{pdfFileError}</div>}
+                                {/* <br></br> */}
+                                {uploadState == 0 && (<button onClick={btnClick} type="button" class={btn_class}><strong>{text}</strong></button>)}
+
+                                {uploadState == 1 && (<>
+                                    {!pdfFileError?<>
+                                <div class="Assignment_Img ">
+                                    <img src="https://media.istockphoto.com/photos/health-care-billing-statement-with-stethoscope-picture-id1224851166?b=1&amp;k=20&amp;m=1224851166&amp;s=170667a&amp;w=0&amp;h=xBJfeOFCnBG5Z6zgI2OFicnvgMF-idwwu3TuRvtq1y8=" alt="" />
+                                    <div class="Assignment_Name">
+                                        {inputFile ? inputFile.name : null}
+                                </div>
+                                    </div></>
+                                    : null
+                                }
+                                <div className="d-flex justify-content-between">
+                                    <button onClick={btnClick} type="button" class={btn_class}><strong>{text}</strong></button>
+                                    <button onClick={btnClick_back} type="button" class={btn_class_1}><strong>Resubmit</strong></button>
+                                </div>
+                                </>)
+                                }
+                                
+                                {uploadState == 2 && (<>
+                                <div class="Assignment_Img">
+                                    <img src="https://media.istockphoto.com/photos/health-care-billing-statement-with-stethoscope-picture-id1224851166?b=1&amp;k=20&amp;m=1224851166&amp;s=170667a&amp;w=0&amp;h=xBJfeOFCnBG5Z6zgI2OFicnvgMF-idwwu3TuRvtq1y8=" alt="" />
+                                    <div class="Assignment_Name">
+                                        {inputFile.name}
+                                    </div>
+                                </div>
+                                <button onClick={btnClick} type="button" class={btn_class}><strong>{text}</strong></button> </>)
+                                }
+                            </form>
                         </div>
                     </div>
                 </div>
