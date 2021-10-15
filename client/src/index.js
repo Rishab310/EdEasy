23600706dd3a8787224fc87a7ce08f57d23ca027
Rishab Goyal <rishabgoyal432@gmail.com>
2021-10-15 18:52:06 +0530

    AuthSlices

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from "react-redux";
+import store from './reduxStore/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <Provider store={store}><App /></Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
