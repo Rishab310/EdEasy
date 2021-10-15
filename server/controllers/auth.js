92e0504aa071bb847676242f09297815f3f2797a
jatin831 <jatinbajaj2001@gmail.com>
2021-10-16 02:15:21 +0530

    server updates

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -84,3 +84,16 @@ exports.signin = (req, res, next) => {
         next(err);
     })
 }
+
+exports.verifyToken = (req, res, next) => {
+    const token = req.body.token;
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, 'secretKey');
+        res.json({message: "Token verified."});
+    } catch (error) {
+        const err = new Error("Token expired. Please sign in again to continue");
+        err.statusCode = 401;
+        next(err);
+    }
+}
