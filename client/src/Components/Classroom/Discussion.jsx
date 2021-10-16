cb871c9a5bc051a3344b9b2ab5a4517e3801936f
Rishab Goyal <rishabgoyal432@gmail.com>
2021-10-17 01:27:44 +0530

    Discussions Integration

--- client/src/Components/Classroom/Discussion.jsx
+++ client/src/Components/Classroom/Discussion.jsx
@@ -10,6 +10,7 @@ import SendRoundedIcon from '@material-ui/icons/SendRounded';
 import autosize from 'autosize';
 import axios from 'axios';
 import db, { storage } from '../../firebase';
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Discussion = ({classCode}) => {
     const [discussionInput, setDiscussionInput] = useState("");
@@ -18,35 +19,26 @@ const Discussion = ({classCode}) => {
     const userData = useSelector(selectUserData);
     let TextArea = useRef(null);
     let FileInput = useRef(null);
+    const storeData = useSelector(selectUserData);
+    const [loading, setLoading] = useState(false);
+    const [createLoading, setCreateLoading] = useState(false);
 
     useEffect(() => {
         autosize(TextArea);
     }, [])
-
-    useEffect(() => {
-        setPosts([
-            {
-                imgLink: "https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                uploadTimestamp: 1633796940000,
-                authorName: "Manish Dhameja",
-                adminEmail: "123@gmail.com"
-            },
-            {
-                imgLink: null,
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                uploadTimestamp: 1633703340000,
-                authorName: "Rishab Goyal",
-                adminEmail: "1234@gmail.com"
-            },
-            {
-                imgLink: null,
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                uploadTimestamp: 1633530000000,
-                authorName: "Ishan Yadav",
-                adminEmail: "12345@gmail.com"
-            }
-        ])
+    const getDiscussion = async () => {
+        setLoading(true);
+        await axios.post("http://localhost:5000/classes/getDiscussions",{
+            classCode: classCode
+        },{ headers: { Authorization: 'Bearer ' + storeData.token } }
+        ).then ((res) => {
+            console.log(res);
+            setPosts(res.data);
+        }).catch(err => console.log(err.response))
+        setLoading(false);
+    }
+    useEffect( () => {
+        getDiscussion();
     }, [])
 
     const imgPreview = (e) => {
@@ -55,6 +47,7 @@ const Discussion = ({classCode}) => {
     }
 
     const createDiscussion = () => {
+        setCreateLoading(true);
         if (fileInput) {
             const fileName = new Date().getTime() + "-" + fileInput.name;
             const uploadTask = storage.ref(`discussion/${fileName}`).put(fileInput);
@@ -76,9 +69,17 @@ const Discussion = ({classCode}) => {
                   })
                   .then(res => {
                     console.log(res);
+                    // window.location.reload(false);
+                    setDiscussionInput("");
+                    setFileInput(null);
+                    getDiscussion();
+                    setCreateLoading(false);
                   })
                   .catch(err => {
                       console.log(err);
+                      setDiscussionInput("");
+                      setFileInput(null);
+                      setCreateLoading(false);
                   })
               })
         } else {
@@ -93,103 +94,130 @@ const Discussion = ({classCode}) => {
                     Authorization: "Bearer " + userData.token
                 }
             })
+            .then(() => {
+                // window.location.reload(false);
+                setDiscussionInput("");
+                setFileInput(null);
+                getDiscussion();
+                setCreateLoading(false);
+            }).catch(err => {
+                console.log(err);
+                setDiscussionInput("");
+                setFileInput(null);
+                setCreateLoading(false);
+            })
         }
     }
 
     return (
         <div>
             <div className="Discussion d-flex py-2 px-3 content-box">
-                <div className="Avatar_Container mt-1 mt-md-0">
-                    <Avatar>
-                        M
-                    </Avatar>
-                </div>
-                <div className="Discussion_TextArea d-flex flex-column justify-content-center align-items-center">
-                    <textarea 
-                        ref={c => (TextArea = c)}
-                        placeholder="Start a discussion, share class materials, etc...."
-                        rows={1}
-                        value={discussionInput}
-                        onChange={(e) => setDiscussionInput(e.target.value)}
+                <>
+                    <div className="Avatar_Container mt-1 mt-md-0">
+                        <Avatar>M</Avatar>
+                    </div>
+                    <div className="Discussion_TextArea d-flex flex-column justify-content-center align-items-center">
+                        <textarea 
+                            ref={c => (TextArea = c)}
+                            placeholder="Start a discussion, share class materials, etc...."
+                            rows={1}
+                            value={discussionInput}
+                            onChange={(e) => setDiscussionInput(e.target.value)}
+                            />
+                        {
+                            fileInput ? (
+                                <div className="Discussion_PreviewImg ms-1 mt-2">
+                                    <img src={URL.createObjectURL(fileInput)} />
+                                </div>
+                            ) : null
+                        }
+                    </div>
+                    <div className="mt-2">
+                        <PhotoRoundedIcon className="InputImgButton" style={{fontSize: "30px"}} onClick={() => FileInput.current.click()}/>
+                        <input 
+                            accept=".jpeg, .jpg, .png"
+                            className="Discussion_FileInput" 
+                            type="file" 
+                            ref={FileInput} 
+                            onChange={(e) => imgPreview(e)}
                         />
+                    </div>
                     {
-                        fileInput ? (
-                            <div className="Discussion_PreviewImg ms-1 mt-2">
-                                <img src={URL.createObjectURL(fileInput)} />
+                        (discussionInput !== '' || fileInput) ? (
+                            <div className="mt-2">
+                                <SendRoundedIcon onClick={createDiscussion} className="SendButton" style={{fontSize: "30px"}}/>
                             </div>
                         ) : null
                     }
-                </div>
-                <div className="mt-2">
-                    <PhotoRoundedIcon className="InputImgButton" style={{fontSize: "30px"}} onClick={() => FileInput.current.click()}/>
-                    <input 
-                        accept=".jpeg, .jpg, .png"
-                        className="Discussion_FileInput" 
-                        type="file" 
-                        ref={FileInput} 
-                        onChange={(e) => imgPreview(e)}
-                    />
-                </div>
-                {
-                    (discussionInput !== '' || fileInput) ? (
-                        <div className="mt-2">
-                            <SendRoundedIcon onClick={createDiscussion} className="SendButton" style={{fontSize: "30px"}}/>
-                        </div>
-                    ) : null
-                }
-                
-
+                </>
             </div>
-            <div className="Posts">
-                {
-                    posts.map(post => {
-                        return (
-                            <div className="content-box px-3 py-2 my-3">
-                                <div className="d-flex">
-                                    <div className="Avatar_Container">
-                                        <Avatar>
-                                            {post.authorName[0]}
-                                        </Avatar>
-                                    </div>
-                                    <div className="Post_Author d-flex flex-column justify-content-center mx-3">
-                                        <div className="Post_AuthorName">{post.authorName}</div>
-                                        {
-                                            post.adminEmail === "123@gmail.com" ? (
-                                                <div className="Post_AdminName">
-                                                    Admin
+            {
+                (createLoading) ? (
+                    <div className="col-12 d-flex justify-content-center align-items-center mt-2">
+                        <CircularProgress size={50} className="display-block"/>
+                    </div>
+                ) : (
+                    <></>
+                )
+            }
+            {
+                (loading) ? (
+                    <div className="col-12 d-flex justify-content-center align-items-center mt-5">
+                        <CircularProgress size={50} className="display-block"/>
+                    </div>
+                ) : (
+                    <div className="Posts">
+                        {
+                            posts.reverse().map(post => {
+                                return (
+                                    <div className="content-box px-3 py-2 my-3">
+                                        <div className="d-flex">
+                                            <div className="Avatar_Container">
+                                                <Avatar>
+                                                    {post.creatorName[0]}
+                                                </Avatar>
+                                            </div>
+                                            <div className="Post_Author d-flex flex-column justify-content-center mx-3">
+                                                <div className="Post_creatorName">{post.creatorName}</div>
+                                                {
+                                                    post.creatorEmail === "123@gmail.com" ? (
+                                                        <div className="Post_AdminName">
+                                                            Admin
+                                                        </div>
+                                                    ) : null
+                                                }
+                                            </div>
+                                            <div className="d-flex flex-column align-items-end">
+                                                <div className="Post_UploadDate">
+                                                    {getDateStringFromTimestamp(post.createdAt)}
                                                 </div>
-                                            ) : null
-                                        }
-                                    </div>
-                                    <div className="d-flex flex-column align-items-end">
-                                        <div className="Post_UploadDate">
-                                            {getDateStringFromTimestamp(post.uploadTimestamp)}
-                                        </div>
-                                        <div className="Post_UploadTime">
-                                            {getTimeFromTimestamp(post.uploadTimestamp)}
+                                                <div className="Post_UploadTime">
+                                                    {getTimeFromTimestamp(post.createdAt)}
+                                                </div>
+                                                <div>
+
+                                                </div>
+                                            </div>
                                         </div>
                                         <div>
-
+                                            {
+                                                post.imgLink ? (
+                                                    <div className="Post_Img">
+                                                        <img src={post.imgLink} alt="" />
+                                                    </div>
+                                                ) : null
+                                            }
+                                            <p className="Post_Desc">
+                                                {post.desc}
+                                            </p>
                                         </div>
                                     </div>
-                                </div>
-                                <div>
-                                    {
-                                        post.imgLink ? (
-                                            <div className="Post_Img">
-                                                <img src={post.imgLink} alt="" />
-                                            </div>
-                                        ) : null
-                                    }
-                                    <p className="Post_Desc">
-                                        {post.description}
-                                    </p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
