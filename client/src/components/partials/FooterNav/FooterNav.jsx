8e5e7e14cae250fca0d34d413a644fd4556cb8ec
Rishab Goyal <rishabgoyal432@gmail.com>
2021-11-14 19:05:49 +0530

    About change

--- client/src/components/partials/FooterNav/FooterNav.jsx
+++ client/src/components/partials/FooterNav/FooterNav.jsx
@@ -7,21 +7,21 @@ function FooterNav() {
       <div className="footer-nav">
         <div className="container">
           <div className="row py-1">
-            <div className="col-4 d-flex justify-content-center">
+            <div className="col-6 d-flex justify-content-center">
               <h3>
                 <NavLink to="/" className="nav-link footer-nav-link" activeClassName="selected" exact>
                   Home
                 </NavLink>
               </h3>
             </div>
-            <div className="col-4 d-flex justify-content-center">
+            {/* <div className="col-4 d-flex justify-content-center">
               <h3>
                 <NavLink to="/classes/reminders" className="nav-link footer-nav-link" activeClassName="selected" exact>
                   Reminders
                 </NavLink>
               </h3>
-            </div>
-            <div className="col-4 d-flex justify-content-center">
+            </div> */}
+            <div className="col-6 d-flex justify-content-center">
               <h3>
                 <NavLink to="/classes" className="nav-link footer-nav-link" activeClassName="selected" >
                   Classes
